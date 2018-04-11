0e82ce6e14c98bc14bf199751b18d5f4d9deb99b
gnaga georges <gnagageorges@gmail.com>
2018-04-10 23:57:28 -0400

    tp2 version provisoire

--- tp2/main.py
+++ tp2/main.py
@@ -35,7 +35,10 @@ def afficherLesMotsDePasse():
 def menu(erreur):
     get = None
     if erreur:
-        print("Cette fonction n'existe pas!")
+        print("Cette fonction n'existe pas! veillez entrer une fonction valide")
+        print(" Exemple: entrer 'a' pour creer un automate\n")
+
+    print("="*20 +" Menu "+20*"=")
     print("Que voulez vous faire?")
     print("  (a) Creer l'automates")
     print("  (b) Traiter des requetes")
@@ -43,44 +46,89 @@ def menu(erreur):
     print("  (d) Quitter")
     return input("")
 
+def VariantesFichiers():
+    print("\nliste de fichiers de variantes disponible \n")
+    print("=> variantes1.txt")
+    print("=> variantes2.txt")
+    print("=> variantes3.txt")
+    print("=> variantes4.txt")
+    print("=> variantes5.txt")
+
+def reglesFichiers():
+    print("\nliste de Fichiers de regles disponible \n")
+    print("=> regles1.txt")
+    print("=> regles2.txt")
+    print("=> regles3.txt")
+    print("=> regles4.txt")
+    print("=> regles5.txt")
 
 def ln():
     print("\n" * 80)
 
 
+def verificationVFichiers(file_name):
+        if(file_name == "variantes1.txt" or file_name == "variantes2.txt" or file_name == "variantes3.txt" or file_name == "variantes4.txt" or file_name == "variantes5.txt" ):
+            return True
+        return False
+
+
+def verificationRFichiers(file_name):
+        if(file_name == "regles1.txt" or file_name == "regles2.txt" or file_name == "regles3.txt" or file_name == "regles4.txt"  or file_name == "regles5.txt" ):
+            return True
+        return False
+
+
+
 automates = {}
 solutions = {}
 get = ""
+repR = False
+repV= False
 erreur = False
 while get != "d":
     get = menu(erreur)
     if get == 'a':
         erreur = False
-        file_name = input("\nQuel est le nom du fichier de regles : ")
-        while 'regles' not in file_name:
-            print("\nil ne s'agit pas d'un fichier de regles")
-            file_name = input("Quel est le nom du fichier de regles : ")
+        reglesFichiers()
+        file_name = input("\n veillez entrer le nom du fichier choisi :: ")
+        repR = verificationRFichiers(file_name)
+        while repR == False:
+            print(
+                "\nil ne s'agit pas d'un fichier de regles, veillez entrez un bon fichier svp")
+            reglesFichiers()
+            file_name = input("veillez entrer le nom du fichier choisi :: ")
+            repR = verificationRFichiers(file_name)
         automate = creerAutomate(file_name)
         automates[automate.name] = automate
+        print("\n=> super! vous pouvez maintenant traiter des requetes en entrant 'b' ")
 
     elif get == 'b':
         erreur = False
-        file_name = input("\nQuel est le nom du fichier de variantes : ")
-        while 'variantes' not in file_name:
-            print("\nil ne s'agit pas d'un fichier de variantes")
-            file_name = input("Quel est le nom du fichier de variantes : ")
+        VariantesFichiers()
+        file_name = input("\n veillez entrer le nom du fichier choisi :: ")
+        repV = verificationVFichiers(file_name)
+        while repV == False:
+            print("\nil ne s'agit pas d'un fichier de variante, veillez entrez un bon fichier svp")
+            VariantesFichiers()
+            file_name = input("veillez entrer le nom du fichier choisi : ")
+            repV = verificationVFichiers(file_name)
         solution = traiterLesEntrees(file_name)
         solutions[solution.no] = solution
+        print("\n=> super! vous pouvez maintenant afficher les mots de passe en entrant 'c' ")
         if solution.no not in automates:
-            input("Aucun automate n'existe pour cette solution!\nConfirmer la reception")
+            input("\nAucun automate n'existe pour cette solution ,veillez en creer un d'abord! ")
         else:
             solutions[solution.no].reponse = trouverMotDePasse(
                 solutions[solution.no].variantes, solution.no)
 
     elif get == 'c':
         erreur = False
-        afficherLesMotsDePasse()
-        input()
+        if repR == True and repV == True :
+            print("\nMot de passe : ")
+            afficherLesMotsDePasse()
+            input("\n appuyer la touche entrez")
+        else:
+            print(" => veillez creer des atomates et traiter des requette d'abord. Merci! ")
 
     else:
         erreur = True
